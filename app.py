f2812eddcee55ae85d1b28e99e44b78213979fc4
Sanjay Nair <nair.sanjay1103@gmail.com>
2021-11-15 11:11:54 +0530

    Change in styling of webpage

--- app.py
+++ app.py
@@ -7,26 +7,8 @@ from sklearn.ensemble.forest import ForestClassifier, ForestRegressor
 
 app = Flask(__name__)
 
-def predict(values, dic):
-
-    values = np.asarray(values)
-    list=[];
-    model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
-    list.append(model.predict(values.reshape(1, -1))[0])
-    model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
-    list.append(model.predict(values.reshape(1, -1))[0])
-    model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
-    list.append(model.predict(values.reshape(1, -1))[0])
-    model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
-    list.append(model.predict(values.reshape(1, -1))[0])
-    model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
-    list.append(model.predict(values.reshape(1, -1))[0])
-    return list
 
 @app.route("/")
-def home():
-    return render_template('diabetes.html')
-
 @app.route("/diabetes", methods=['GET', 'POST'])
 def diabetesPage():
     return render_template('diabetes.html')
@@ -34,13 +16,26 @@ def diabetesPage():
 
 @app.route("/predict", methods = ['POST', 'GET'])
 def predictPage():
-    if request.method == 'POST':
-        to_predict_dict = request.form.to_dict()
-        to_predict_list = list(map(float, list(to_predict_dict.values())))
-        pred = predict(to_predict_list, to_predict_dict)
-
-    return render_template('predict.html', pred = pred)
+    try:
+        if request.method == 'POST':
+            predict_dict = request.form.to_dict()
+            predict_list = list(map(float, list(predict_dict.values())))
+            predict_list = np.asarray(predict_list)
+            list_predicted = []
+            model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
+            list_predicted.append(model.predict(predict_list.reshape(1, -1))[0])
+            model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
+            list_predicted.append(model.predict(predict_list.reshape(1, -1))[0])
+            model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
+            list_predicted.append(model.predict(predict_list.reshape(1, -1))[0])
+            model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
+            list_predicted.append(model.predict(predict_list.reshape(1, -1))[0])
+            model = pickle.load(open('models/Random_Forest.pkl', 'rb'))
+            list_predicted.append(model.predict(predict_list.reshape(1, -1))[0])
+    except:
+        return render_template("diabetes.html", message='Invalid Data')
+    return render_template('predict.html', pred=list_predicted)
 
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug = True)
